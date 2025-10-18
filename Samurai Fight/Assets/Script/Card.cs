ebf2a0fa6e0b62f824649c8bbb4a93347a5cdbe7
eggmals <makmal.farrel@gmail.com>
2025-10-18 11:39:39 +0700

    update

--- Samurai Fight/Assets/Script/Card.cs
+++ Samurai Fight/Assets/Script/Card.cs
@@ -1,14 +1,12 @@
-[System.Serializable] // Membuat data kelas ini bisa dilihat di Unity Inspector.
+[System.Serializable]
 public class Card
 {
-    // Variabel untuk menyimpan nilai kartu (misalnya: 1, 2, 3, 4, atau 5).
+
     public int value;
 
-    // Constructor: Fungsi yang dipanggil saat sebuah objek Card baru dibuat.
     public Card(int val)
     {
-        // 'this.value' menunjuk ke variabel 'value' di dalam kelas ini.
-        // 'val' adalah nilai yang dikirim saat membuat kartu baru.
+
         this.value = val;
     }
 }
