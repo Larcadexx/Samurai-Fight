f523cc5bd521089fa22894d04398bcb315126f03
eggmals <makmal.farrel@gmail.com>
2025-10-21 00:56:14 +0700

    new petak

--- Samurai Fight/Assets/Script/GameManager.cs
+++ Samurai Fight/Assets/Script/GameManager.cs
@@ -545,7 +545,7 @@ public class GameManager : MonoBehaviour
         if (targetPosition > 0 && targetPosition <= petakPapan.Length)
         {
             Transform targetPetak = petakPapan[targetPosition - 1];
-            pawn.transform.position = targetPetak.position + new Vector3(0, 100f, 0);
+            pawn.transform.position = targetPetak.position + new Vector3(0, 105f, 0);
         }
     }
 
