a383c09c2c185f7b54ea83866a01e2aedd7a25d4
eggmals <makmal.farrel@gmail.com>
2025-10-23 17:46:31 +0700

    button audio

---
+++ Samurai Fight/Assets/Script/TombolAnimasi.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections; 
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(AudioSource))] 
+public class TombolAnimasi : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [Header("Pengaturan Animasi")]
+    public float scaleHover = 1.1f;
+    public float scaleTekan = 0.95f;
+    public float posisiHoverY = 10f; 
+    public float durasiAnimasi = 0.15f; 
+
+    [Header("Pengaturan Audio")]
+    public AudioClip audioKlik; 
+
+    private Vector3 originalScale;
+    private Vector3 originalPosition;
+    private Button myButton;
+    private AudioSource audioSource; 
+    private Coroutine animasiBerjalan;
+
+    void Awake()
+    {
+        originalScale = transform.localScale;
+        originalPosition = transform.localPosition;
+        myButton = GetComponent<Button>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (myButton != null && myButton.interactable)
+        {
+            Vector3 targetScale = originalScale * scaleHover;
+            Vector3 targetPos = originalPosition + new Vector3(0, posisiHoverY, 0);
+            MulaiAnimasi(targetScale, targetPos);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MulaiAnimasi(originalScale, originalPosition);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (myButton != null && myButton.interactable)
+        {
+            Vector3 targetScale = originalScale * scaleTekan;
+            Vector3 targetPos = originalPosition + new Vector3(0, posisiHoverY, 0);
+            MulaiAnimasi(targetScale, targetPos);
+            if (audioKlik != null)
+            {
+                audioSource.PlayOneShot(audioKlik);
+            }
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (myButton != null && myButton.interactable)
+        {
+            RectTransform rect = GetComponent<RectTransform>();
+            if (RectTransformUtility.RectangleContainsScreenPoint(rect, eventData.position, eventData.pressEventCamera))
+            {
+                Vector3 targetScale = originalScale * scaleHover;
+                Vector3 targetPos = originalPosition + new Vector3(0, posisiHoverY, 0);
+                MulaiAnimasi(targetScale, targetPos);
+            }
+            else
+            {
+                MulaiAnimasi(originalScale, originalPosition);
+            }
+        }
+    }
+
+    private void MulaiAnimasi(Vector3 targetScale, Vector3 targetPosition)
+    {
+        if (animasiBerjalan != null)
+        {
+            StopCoroutine(animasiBerjalan);
+        }
+        animasiBerjalan = StartCoroutine(Animasikan(targetScale, targetPosition));
+    }
+
+    private IEnumerator Animasikan(Vector3 targetScale, Vector3 targetPosition)
+    {
+        float waktu = 0;
+        Vector3 startScale = transform.localScale;
+        Vector3 startPos = transform.localPosition;
+
+        while (waktu < durasiAnimasi)
+        {
+            float t = waktu / durasiAnimasi;
+            transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            transform.localPosition = Vector3.Lerp(startPos, targetPosition, t);
+            waktu += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
+        transform.localPosition = targetPosition;
+        animasiBerjalan = null;
+    }
+
+    void OnDisable()
+    {
+        if (animasiBerjalan != null)
+        {
+            StopCoroutine(animasiBerjalan);
+        }
+        transform.localScale = originalScale;
+        transform.localPosition = originalPosition;
+    }
+}
