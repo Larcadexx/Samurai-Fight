c2fb7f5dec091e6e4d9c5a5d874c0b1896a9555d
eggmals <makmal.farrel@gmail.com>
2025-10-27 19:44:45 +0700

    update

--- Samurai Fight/Assets/Script/Menu/MenuManager.cs
+++ Samurai Fight/Assets/Script/Menu/MenuManager.cs
@@ -8,24 +8,30 @@ public class MenuManager : MonoBehaviour
     public GameObject playButton;
     public GameObject gameRuleButton;
     public GameObject creditButton;
-
+    public GameObject closeAppButton;     
+    public GameObject volumeButton;        
 
     [Header("Panel")]
     public GameObject CreditPanel;
     public GameObject GameRulePanel;
+    public GameObject VolumePanel;        
 
     [Header("Pages")]
     public List<GameObject> Pages;
     private int currentPageIndex = 0;
+
+    private bool isVolumePanelVisible = false;
 
     void Start()
     {
         playButton.SetActive(true);
         creditButton.SetActive(true);
         gameRuleButton.SetActive(true);
-
+        closeAppButton.SetActive(true);
+        volumeButton.SetActive(true);
         CreditPanel.SetActive(false);
         GameRulePanel.SetActive(false);
+        VolumePanel.SetActive(false);
     }
 
     public void PlayGame()
@@ -35,41 +41,27 @@ public class MenuManager : MonoBehaviour
 
     public void ShowCredits()
     {
-        playButton.SetActive(false);
-        creditButton.SetActive(false);
-        gameRuleButton.SetActive(false);
-
+        ToggleMainButtons(false);
         CreditPanel.SetActive(true);
     }
 
     public void HideCredits()
     {
-        playButton.SetActive(true);
-        creditButton.SetActive(true);
-        gameRuleButton.SetActive(true);
-
+        ToggleMainButtons(true);
         CreditPanel.SetActive(false);
     }
 
     public void ShowGameRules()
     {
-        playButton.SetActive(false);
-        creditButton.SetActive(false);
-        gameRuleButton.SetActive(false);
-        
+        ToggleMainButtons(false);
         GameRulePanel.SetActive(true);
-
         currentPageIndex = 0;
         UpdatePageDisplay();
     }
-    
 
     public void HideGameRules()
     {
-        playButton.SetActive(true);
-        creditButton.SetActive(true);
-        gameRuleButton.SetActive(true);
-
+        ToggleMainButtons(true);
         GameRulePanel.SetActive(false);
     }
 
@@ -77,9 +69,7 @@ public class MenuManager : MonoBehaviour
     {
         currentPageIndex++;
         if (currentPageIndex >= Pages.Count)
-        {
             currentPageIndex = 0;
-        }
         UpdatePageDisplay();
     }
 
@@ -87,17 +77,38 @@ public class MenuManager : MonoBehaviour
     {
         currentPageIndex--;
         if (currentPageIndex < 0)
-        {
             currentPageIndex = Pages.Count - 1;
-        }
         UpdatePageDisplay();
     }
 
     void UpdatePageDisplay()
     {
         for (int i = 0; i < Pages.Count; i++)
-        {
             Pages[i].SetActive(i == currentPageIndex);
-        }
+    }
+
+    public void CloseApp()
+    {
+        Debug.Log("Menutup aplikasi...");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  
+        #else
+        Application.Quit();  
+        #endif
+    }
+
+    public void ToggleVolumePanel()
+    {
+        isVolumePanelVisible = !isVolumePanelVisible;
+        VolumePanel.SetActive(isVolumePanelVisible);
+    }
+
+    private void ToggleMainButtons(bool visible)
+    {
+        playButton.SetActive(visible);
+        creditButton.SetActive(visible);
+        gameRuleButton.SetActive(visible);
+        closeAppButton.SetActive(visible);
+        volumeButton.SetActive(visible);
     }
 }
